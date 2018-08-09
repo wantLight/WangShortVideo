c7e52fd070168a376bade66fe6e704f6080477be
xyzzg <1364932323@qq.com>
2018-08-09 18:16:37 +0800

    完成分层聚合项目初始化，以及使用mybatis-generator生成相应文件，使用maven install成功安装项目。

---
+++ imooc-videos-dev/imooc-videos-dev-service/src/main/java/com/imooc/ImoocVideosDevServiceApplication.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocVideosDevServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocVideosDevServiceApplication.class, args);
+	}
+}
